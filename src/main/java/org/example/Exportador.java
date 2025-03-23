b9873124e2be01382c04b0edffb735ffaa91fcbf
Carla Berón <alracnoreb@gmail.com>
2025-03-22 21:33:10 -0300

    tp 2 p2

--- src/main/java/org/example/Exportador.java
+++
@@ -1,5 +0,0 @@
-package unrn.export;
-
-public interface Exportador {
-    void export(String data);
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         var jose = new Participante("234566", "Jose Perez", 1);
-        RegistroInscriptos registro = new RegistroInscriptosDAO();
+        RegistroInscriptos registro = new ArchivoDeInscriptos("F:\\proyectos\\sistemas\\materias2025\\primer cuatrimestre\\orientacion a objetos II\\carpeta\\archivoInscriptos.txt");
+//        RegistroInscriptos registro = new RegistroInscriptosDAO();
         LocalDate fechaInscripcion = LocalDate.of(2025, 3, 15);
         LocalDate fechaInicio = LocalDate.of(2025, 3, 1);
         LocalDate fechaFin = LocalDate.of(2025, 3, 31);
